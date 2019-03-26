3010702e0cc17d335461d7e59ff2ec6fa90cc8e4
Javier Lete <jglete@gmail.com>
2019-03-26 18:42:31 +0100

    Generación componente ProductosListadoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosListadoComponent } from './productos-listado/productos-listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosListadoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/productos-listado/productos-listado.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductosListadoComponent } from './productos-listado.component';
+
+describe('ProductosListadoComponent', () => {
+  let component: ProductosListadoComponent;
+  let fixture: ComponentFixture<ProductosListadoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductosListadoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductosListadoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
